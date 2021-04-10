90a7889f217a847391e852eeb6b4f751c81f127a
Dinara <dinaramamatkazina@gmail.com>
2021-04-10 16:00:00 +0300

    adde edit form

--- src/modules/users/components/UsersList.js
+++ src/modules/users/components/UsersList.js
@@ -2,11 +2,16 @@ import React from "react";
 import UserListItem from "./UserListItem";
 import { Container } from "@material-ui/core";
 
-function UsersList({ list }) {
+function UsersList({ list, onSelect, onDelete }) {
   return (
     <Container maxWidth="sm">
       {list.map((user) => (
-        <UserListItem key={user.id} item={user} />
+        <UserListItem
+          key={user.id}
+          user={user}
+          onSelect={onSelect}
+          onDelete={onDelete}
+        />
       ))}
     </Container>
   );
